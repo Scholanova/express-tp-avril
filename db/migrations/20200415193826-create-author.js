8517140d0cb89ac66e43954d67a4a8119588ee0e
djibril <djibril@appndigital.com>
2020-04-16 15:11:14 +0200

    refacto circleci

--- db/migrations/20200415193826-create-author.js
+++ db/migrations/20200415193826-create-author.js
@@ -21,9 +21,6 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       },
-      language: {
-              type: Sequelize.STRING
-       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
