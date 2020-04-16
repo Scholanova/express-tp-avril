b8551463ff01c51a95fc2cab5d3550284cb15063
Namelistone <namelistone@gmail.com>
2020-04-16 11:03:34 +0200

    Etape 4 - migration

---
+++ db/migrations/etape4.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'Authors',
+      'language',
+      Sequelize.STRING 
+    );
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Authors');
+  }
+};
