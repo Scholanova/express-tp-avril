4647e97f2b2b2985a0c115de90e4585e6ede5aca
Florian <florianbematol@gmail.com>
2020-04-16 15:51:21 +0200

    Partie 3 - Etape 10

--- test/repositories/authorRepositoryTest.js
+++ test/repositories/authorRepositoryTest.js
@@ -1,168 +1,168 @@
-const { expect } = require('../testHelper')
-
-const authorRepository = require('../../lib/repositories/authorRepository')
-const models = require('../../lib/models')
-const { ResourceNotFoundError } = require('../../lib/errors')
-const Author = models.Author
-
-describe('authorRepository', () => {
-
-  afterEach(async () => {
-    await Author.destroy({ where: {} })
-  })
-
-  describe('get', () => {
-
-    let notExistingId
-    let getAuthorPromise
-
-    context('author does not exist', () => {
-      beforeEach(async () => {
-        // given
-        notExistingId = 23456789
-
-        // when
-        getAuthorPromise = authorRepository.get(notExistingId)
-      })
-
-      it('should throw a not found error', () => {
-        // then
-        return expect(getAuthorPromise).to.eventually.be.rejectedWith(ResourceNotFoundError)
-      })
-    })
-  })
-
-  describe('create', () => {
-
-    let createdAuthor
-    let retrievedAuthor
-    let authorData
-
-    beforeEach(async () => {
-      // given
-      authorData = { name: 'Jean-Paul Sartre', pseudo: undefined, email: 'jp_sartre@academie-francaise.fr', language: 'french' }
-
-      // when
-      createdAuthor = await authorRepository.create(authorData)
-    })
-
-    // then
-    it('should return a author with the right properties', async () => {
-      const createdAuthorValue = createdAuthor.get()
-
-      expect(createdAuthorValue.name).to.equal(authorData.name)
-      expect(createdAuthorValue.age).to.equal(authorData.age)
-      expect(createdAuthorValue.language).to.equal(authorData.language)
-
-      retrievedAuthor = await authorRepository.get(createdAuthor.id)
-      const retrievedAuthorValue = retrievedAuthor.get()
-
-      expect(createdAuthorValue).to.deep.equal(retrievedAuthorValue)
-    })
-  })
-
-  describe('listAll', () => {
-    let result
-
-    context('when there is no authors in the repository', () => {
-
-      beforeEach(async () => {
-        // given
-
-        // when
-        result = await authorRepository.listAll()
-      })
-
-      it('should return an empty list', () => {
-        // then
-        expect(result).to.be.empty
-      })
-    })
-
-    context('when there are two authors in the repository', () => {
-
-      let author1
-      let author2
-
-      beforeEach(async () => {
-        // given
-        const jjrData = { name: 'Jean-Jacques Rousseau', pseudo: 'JJR', email: 'jj@rousseau.ch', language: 'french' }
-        const ppData = { name: 'Philip Pullman', pseudo: 'Philip', email: 'philip@pullman.co.uk', language: 'english' }
-        author1 = await authorRepository.create(jjrData)
-        author2 = await authorRepository.create(ppData)
-
-        // when
-        result = await authorRepository.listAll()
-      })
-
-      it('should return a list with the two authors', () => {
-        // then
-        const author1Value = author1.get()
-        const author2Value = author2.get()
-        const resultValues = result.map((author) => author.get())
-
-        expect(resultValues).to.deep.equal([author1Value, author2Value])
-      })
-    })
-  })
-
-  describe('listForLanguage', () => {
-
-    let result
-
-    context('when there is no authors for that language in the repository, only some for other language', () => {
-
-      beforeEach(async () => {
-        // given
-        const jjrData = { name: 'Jean-Jacques Rousseau', pseudo: 'JJR', email: 'jj@rousseau.ch', language: 'french' }
-        const ppData = { name: 'Philip Pullman', pseudo: 'Philip', email: 'philip@pullman.co.uk', language: 'french' }
-        author1 = await authorRepository.create(jjrData)
-        author2 = await authorRepository.create(ppData)
-
-        // when
-        result = await authorRepository.listForLanguage('english');
-      })
-
-      it('should return an empty list', () => {
-        // then
-        expect(result).to.be.empty;
-      })
-    })
-
-    context('when there are two authors in the repository for that language and some for other languages', () => {
-
-      let author1
-      let author2
-      let author3
-      let author4
-      let author5
-
-      beforeEach(async () => {
-        // given
-        const jjrData = { name: 'Jean-Jacques Rousseau', pseudo: 'JJR', email: 'jj@rousseau.ch', language: 'french' }
-        const ppData = { name: 'Philip Pullman', pseudo: 'Philip', email: 'philip@pullman.co.uk', language: 'french' }
-        const eaaData = { name: 'Edwin Abbott Abbott', pseudo: 'E. Bott', email: 'edwin@abbott.en', language: 'english' }
-        const paData = { name: 'Paul Ableman', pseudo: 'PA', email: 'paul@ableman.en', language: 'english' }
-        const haData = { name: 'Harriet Arbuthnot', pseudo: 'Arbut', email: 'harriet@arbuthnot.co.uk', language: 'english' }
-        author1 = await authorRepository.create(jjrData)
-        author2 = await authorRepository.create(ppData)
-        author3 = await authorRepository.create(eaaData)
-        author4 = await authorRepository.create(paData)
-        author5 = await authorRepository.create(haData)
-
-        // when
-        result = await authorRepository.listForLanguage('french')
-      })
-
-      it('should return a list with the two authors', () => {
-        // then
-        const author1Value = author1.get()
-        const author2Value = author2.get()
-        const resultValues = result.map((author) => author.get())
-
-        expect(resultValues).to.deep.equal([author1Value, author2Value])
-      })
+  const { expect } = require('../testHelper')
+  
+  const authorRepository = require('../../lib/repositories/authorRepository')
+  const models = require('../../lib/models')
+  const { ResourceNotFoundError } = require('../../lib/errors')
+  const Author = models.Author
+  
+  describe('authorRepository', () => {
+    
+    afterEach(async () => {
+        await Author.destroy({ where: {} })
     })
     
-  })
+    describe('get', () => {
+        
+        let notExistingId
+        let getAuthorPromise
+        
+        context('author does not exist', () => {
+            beforeEach(async () => {
+                // given
+                notExistingId = 23456789
+                
+                // when
+                getAuthorPromise = authorRepository.get(notExistingId)
+            })
+            
+            it('should throw a not found error', () => {
+                // then
+                return expect(getAuthorPromise).to.eventually.be.rejectedWith(ResourceNotFoundError)
+            })
+        })
+    })
+    
+    describe('create', () => {
+        
+        let createdAuthor
+        let retrievedAuthor
+        let authorData
+        
+        beforeEach(async () => {
+            // given
+            authorData = { name: 'Jean-Paul Sartre', pseudo: undefined, email: 'jp_sartre@academie-francaise.fr', language: 'french' }
+            
+            // when
+            createdAuthor = await authorRepository.create(authorData)
+        })
+        
+        // then
+        it('should return a author with the right properties', async () => {
+            const createdAuthorValue = createdAuthor.get()
+            
+            expect(createdAuthorValue.name).to.equal(authorData.name)
+            expect(createdAuthorValue.age).to.equal(authorData.age)
+            expect(createdAuthorValue.language).to.equal(authorData.language)
+            
+            retrievedAuthor = await authorRepository.get(createdAuthor.id)
+            const retrievedAuthorValue = retrievedAuthor.get()
+            
+            expect(createdAuthorValue).to.deep.equal(retrievedAuthorValue)
+        })
+    })
+    
+    describe('listAll', () => {
+        let result
+        
+        context('when there is no authors in the repository', () => {
+            
+            beforeEach(async () => {
+                // given
+                
+                // when
+                result = await authorRepository.listAll()
+            })
+            
+            it('should return an empty list', () => {
+                // then
+                expect(result).to.be.empty
+            })
+        })
+        
+        context('when there are two authors in the repository', () => {
+            
+            let author1
+            let author2
+            
+            beforeEach(async () => {
+                // given
+                const jjrData = { name: 'Jean-Jacques Rousseau', pseudo: 'JJR', email: 'jj@rousseau.ch', language: 'french' }
+                const ppData = { name: 'Philip Pullman', pseudo: 'Philip', email: 'philip@pullman.co.uk', language: 'english' }
+                author1 = await authorRepository.create(jjrData)
+                author2 = await authorRepository.create(ppData)
+                
+                // when
+                result = await authorRepository.listAll()
+            })
+            
+            it('should return a list with the two authors', () => {
+                // then
+                const author1Value = author1.get()
+                const author2Value = author2.get()
+                const resultValues = result.map((author) => author.get())
+                
+                expect(resultValues).to.deep.equal([author1Value, author2Value])
+            })
+        })
+    })
+    
+    describe('listForLanguage', () => {
+        
+        let result
+        
+        context('when there is no authors for that language in the repository, only some for other language', () => {
+            
+            beforeEach(async () => {
+                // given
+                const jjrData = { name: 'Jean-Jacques Rousseau', pseudo: 'JJR', email: 'jj@rousseau.ch', language: 'french' }
+                const ppData = { name: 'Philip Pullman', pseudo: 'Philip', email: 'philip@pullman.co.uk', language: 'french' }
+                author1 = await authorRepository.create(jjrData)
+                author2 = await authorRepository.create(ppData)
+                
+                // when
+                result = await authorRepository.listForLanguage('english');
+            })
+            
+            it('should return an empty list', () => {
+                // then
+                expect(result).to.be.empty;
+            })
+        })
+        
+        context('when there are two authors in the repository for that language and some for other languages', () => {
+            
+            let author1
+            let author2
+            let author3
+            let author4
+            let author5
+            
+            beforeEach(async () => {
+                // given
+                const jjrData = { name: 'Jean-Jacques Rousseau', pseudo: 'JJR', email: 'jj@rousseau.ch', language: 'french' }
+                const ppData = { name: 'Philip Pullman', pseudo: 'Philip', email: 'philip@pullman.co.uk', language: 'french' }
+                const eaaData = { name: 'Edwin Abbott Abbott', pseudo: 'E. Bott', email: 'edwin@abbott.en', language: 'english' }
+                const paData = { name: 'Paul Ableman', pseudo: 'PA', email: 'paul@ableman.en', language: 'english' }
+                const haData = { name: 'Harriet Arbuthnot', pseudo: 'Arbut', email: 'harriet@arbuthnot.co.uk', language: 'english' }
+                author1 = await authorRepository.create(jjrData)
+                author2 = await authorRepository.create(ppData)
+                author3 = await authorRepository.create(eaaData)
+                author4 = await authorRepository.create(paData)
+                author5 = await authorRepository.create(haData)
+                
+                // when
+                result = await authorRepository.listForLanguage('french')
+            })
+            
+            it('should return a list with the two authors', () => {
+                // then
+                const author1Value = author1.get()
+                const author2Value = author2.get()
+                const resultValues = result.map((author) => author.get())
+                
+                expect(resultValues).to.deep.equal([author1Value, author2Value])
+            })
+        })
+        
+    })
 })
---
+++ test/repositories/bookRepositoryTest.js
@@ -0,0 +1,105 @@
+const { expect } = require('../testHelper')
+
+const bookRepository = require('../../lib/repositories/bookRepository')
+const models = require('../../lib/models')
+const { ResourceNotFoundError } = require('../../lib/errors')
+const Book = models.Book
+const Author = models.Author
+
+describe('bookRepository', () => {
+  
+    afterEach(async () => {
+        await Book.destroy({ where: {} })
+        await Author.destroy({ where: {} })
+    })
+
+    describe('get', () => {
+        
+        let notExistingId
+        let getBookPromise
+        let createdBook
+        
+        context('book does not exist', () => {
+            beforeEach(async () => {
+                // given
+                notExistingId = 23456789
+                
+                // when
+                getBookPromise = bookRepository.get(notExistingId)
+            })
+            
+            it('should throw a not found error', () => {
+                // then
+                return expect(getBookPromise).to.eventually.be.rejectedWith(ResourceNotFoundError)
+            })
+        })
+
+        context('book exist', () => {
+
+            beforeEach(async () => {
+                // given
+                let author = await Author.create({ id: 1234, name: 'Jean-Paul Sartre', pseudo: 'JPP', email: 'jp_sartre@academie-francaise.fr', language: 'french' });
+                createdBook = await Book.create({ authorId: author.id, title: 'L\'aube noir' })
+                
+                // when
+                getBookPromise = bookRepository.get(createdBook.id)
+            })
+
+            it('should return the right book', async () => {
+                let gettedBook = await getBookPromise;
+                let gettedBookValue = gettedBook.get();
+
+                let createdBookValue = createdBook.get();
+
+                expect(createdBookValue).to.deep.equal(gettedBookValue);
+            })
+        })
+    })
+    
+    describe('create', () => {
+
+        let bookData
+        let createdBook
+
+        context('when author not existing', () => {
+
+            beforeEach(async () => {
+                // given
+                bookData = { authorId: 1234, title: 'L\'aube rouge' }
+                            })
+
+            it('should throw an error', () => {
+                // when & then
+                expect(bookRepository.create(bookData)).to.eventually.be.rejected;
+            })
+
+        })
+
+        context('when author is existing', () => {
+
+            beforeEach(async () => {
+                // given
+                let author = await Author.create({ id: 1234, name: 'Jean-Paul Sartre', pseudo: 'JPP', email: 'jp_sartre@academie-francaise.fr', language: 'french' });
+                bookData = { authorId: author.id, title: 'L\'aube rouge' }
+
+                // when
+                createdBook = await bookRepository.create(bookData)
+                let a = await createdBook.getAuthor();
+            })
+
+            it('should return a book with the right properties', () => {
+                const createdBookValue = createdBook.get()
+            
+                expect(createdBookValue.title).to.equal(bookData.title)
+                
+                // retrievedAuthor = await authorRepository.get(createdAuthor.id)
+                // const retrievedAuthorValue = retrievedAuthor.get()
+                
+                // expect(createdAuthorValue).to.deep.equal(retrievedAuthorValue)
+            })
+            
+        })
+
+    })
+
+})
