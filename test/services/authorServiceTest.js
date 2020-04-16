83e8c4936fff114c8459bc4b191e2225e567bea4
DIARRA <dieyna93@gmail.com>
2020-04-16 12:46:12 +0200

    etape 3

--- test/services/authorServiceTest.js
+++ test/services/authorServiceTest.js
@@ -1,35 +1,25 @@
 const { expect, sinon } = require('../testHelper')
-
 const authorService = require('../../lib/services/authorService')
 const authorRepository = require('../../lib/repositories/authorRepository')
 const Joi = require('@hapi/joi')
 const Author = require('../../lib/models').Author
-
 describe('authorService', () => {
-
   describe('create', () => {
-
     let authorData
     let authorCreationPromise
-
     beforeEach(() => {
       sinon.stub(authorRepository, 'create')
     })
-
     context('when the author data is valid', () => {
-
       let author
-
       beforeEach(() => {
         // given
-        authorData = { name: 'Jean-Jacques Rousseau', pseudo: 'JJR', email: 'jj@rousseau.ch' }
+        authorData = { name: 'Jean-Jacques Rousseau', pseudo: 'JJR', email: 'jj@rousseau.ch', language: 'french' }
         author = new Author(authorData)
         authorRepository.create.resolves(author)
-
         // when
         authorCreationPromise = authorService.create(authorData)
       })
-
       // then
       it('should call the author Repository with the creation data', async () => {
         // then
@@ -41,17 +31,13 @@ describe('authorService', () => {
         return expect(authorCreationPromise).to.eventually.equal(author)
       })
     })
-
     context('when the author name is missing', () => {
-
       beforeEach(() => {
         // given
-        authorData = { name: undefined, pseudo: 'JJR', email: 'jj@rousseau.ch' }
-
+        authorData = {  pseudo: 'JJR', email: 'jj@rousseau.ch', language: 'french' }
         // when
         authorCreationPromise = authorService.create(authorData)
       })
-
       it('should not call the author Repository', async () => {
         // then
         await authorCreationPromise.catch(() => {})
@@ -65,23 +51,18 @@ describe('authorService', () => {
           type: 'any.required',
           context: { label: 'name', key: 'name' }
         }]
-
         return expect(authorCreationPromise)
           .to.eventually.be.rejectedWith(Joi.ValidationError)
           .with.deep.property('details', expectedErrorDetails)
       })
     })
-
     context('when the author name is empty', () => {
-
       beforeEach(() => {
         // given
-        authorData = { name: '', pseudo: 'JJR', email: 'jj@rousseau.ch' }
-
+        authorData = { name: '', pseudo: 'JJR', email: 'jj@rousseau.ch', language: 'french' }
         // when
         authorCreationPromise = authorService.create(authorData)
       })
-
       it('should not call the author Repository', async () => {
         // then
         await authorCreationPromise.catch(() => {})
@@ -95,23 +76,18 @@ describe('authorService', () => {
           type: 'string.empty',
           context: { label: 'name', key: 'name', value: '' }
         }]
-
         return expect(authorCreationPromise)
           .to.eventually.be.rejectedWith(Joi.ValidationError)
           .with.deep.property('details', expectedErrorDetails)
       })
     })
-
     context('when the author name is to short', () => {
-
       beforeEach(() => {
         // given
-        authorData = { name: 'JJR', pseudo: 'JJR', email: 'jj@rousseau.ch' }
-
+        authorData = { name: 'JJR', pseudo: 'JJR', email: 'jj@rousseau.ch', language: 'french' }
         // when
         authorCreationPromise = authorService.create(authorData)
       })
-
       it('should not call the author Repository', async () => {
         // then
         await authorCreationPromise.catch(() => {})
@@ -130,17 +106,13 @@ describe('authorService', () => {
           .with.deep.property('details', expectedErrorDetails)
       })
     })
-
     context('when the author email is missing', () => {
-
       beforeEach(() => {
         // given
-        authorData = { name: 'Jean-Jacques', pseudo: 'JJR', email: undefined }
-
+        authorData = { name: 'Jean-Jacques Rousseau', pseudo: 'JJR',  language: 'french' }
         // when
         authorCreationPromise = authorService.create(authorData)
       })
-
       it('should not call the author Repository', async () => {
         // then
         await authorCreationPromise.catch(() => {})
@@ -154,23 +126,18 @@ describe('authorService', () => {
           type: 'any.required',
           context: { label: 'email', key: 'email' }
         }]
-
         return expect(authorCreationPromise)
           .to.eventually.be.rejectedWith(Joi.ValidationError)
           .with.deep.property('details', expectedErrorDetails)
       })
     })
-
     context('when the author email is not a valid email', () => {
-
       beforeEach(() => {
         // given
-        authorData = { name: 'Jean-Jacques', pseudo: 'JJR', email: 'not an email' }
-
+        authorData = { name: 'Jean-Jacques', pseudo: 'JJR', email: 'not an email', language:'french' }
         // when
         authorCreationPromise = authorService.create(authorData)
       })
-
       it('should not call the author Repository', async () => {
         // then
         await authorCreationPromise.catch(() => {})
@@ -185,26 +152,20 @@ describe('authorService', () => {
             context: {
               invalids: ['not an email'], label: 'email', key: 'email', value: 'not an email'
             },
-
           }]
-
           return expect(authorCreationPromise)
             .to.eventually.be.rejectedWith(Joi.ValidationError)
             .with.deep.property('details', expectedErrorDetails)
         }
       )
     })
-
     context('when the author name and email are missing', () => {
-
       beforeEach(() => {
         // given
-        authorData = {}
-
+        authorData = {language:'french'}
         // when
         authorCreationPromise = authorService.create(authorData)
       })
-
       it('should not call the author Repository', async () => {
         // then
         await authorCreationPromise.catch(() => {})
@@ -226,23 +187,18 @@ describe('authorService', () => {
             type: 'any.required'
           }
         ]
-
         return expect(authorCreationPromise)
           .to.eventually.be.rejectedWith(Joi.ValidationError)
           .with.deep.property('details', expectedErrorDetails)
       })
     })
-
-    context.skip('when the author language is missing', () => {
-
+    context('when the author language is missing', () => {
       beforeEach(() => {
         // given
-        authorData = { name: 'JJR', pseudo: 'JJR', email: 'jj@rousseau.ch', language: undefined }
-
+        authorData = { name: 'JeanJR', pseudo: 'JJR', email: 'jj@rousseau.ch' }
         // when
         authorCreationPromise = authorService.create(authorData)
       })
-
       it('should not call the author Repository', async () => {
         // then
         await authorCreationPromise.catch(() => {})
@@ -256,23 +212,18 @@ describe('authorService', () => {
           type: 'any.required',
           context: { label: 'language', key: 'language' }
         }]
-
         return expect(authorCreationPromise)
           .to.eventually.be.rejectedWith(Joi.ValidationError)
           .with.deep.property('details', expectedErrorDetails)
       })
     })
-
     context.skip('when the author language is neither french nor english', () => {
-
       beforeEach(() => {
         // given
-        authorData = { name: 'JJR', pseudo: 'JJR', email: 'jj@rousseau.ch', language: 'german' }
-
+        authorData = { name: 'JJRF', pseudo: 'JJR', email: 'jj@rousseau.ch', language: 'german' }
         // when
         authorCreationPromise = authorService.create(authorData)
       })
-
       it('should not call the author Repository', async () => {
         // then
         await authorCreationPromise.catch(() => {})
@@ -286,19 +237,16 @@ describe('authorService', () => {
           type: 'any.only',
           context: { label: 'language', key: 'language', valids: ['french', 'english'], 'value': 'german' }
         }]
-
         return expect(authorCreationPromise)
           .to.eventually.be.rejectedWith(Joi.ValidationError)
           .with.deep.property('details', expectedErrorDetails)
       })
     })
   })
-
-  describe.skip('listForLanguage', () => {
-    describe.skip('listForLanguage', () => {
+  describe('listForLanguage', () => {
+    describe('listForLanguage', () => {
       let result
-
-      context.skip('when the author language is missing', () => {
+      context('when the author language is missing', () => {
         it('should not call the author Repository', async () => {
           // then
         })
@@ -306,8 +254,7 @@ describe('authorService', () => {
           // then
         })
       })
-      context.skip('when the author language is neither french nor english', () => {
-
+      context('when the author language is neither french nor english', () => {
         it('should not call the author Repository', async () => {
           // then
         })
@@ -315,7 +262,7 @@ describe('authorService', () => {
           // then
         })
       })
-      context.skip('when the author language is either french or english', () => {
+      context('when the author language is either french or english', () => {
         it('should call the author Repository with the language', async () => {
           // then
         })
