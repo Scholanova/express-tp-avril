c9f564307b2d257321fe034cdf39b562ccd82065
LivioChan <ctlivio@yahoo.fr>
2020-04-17 14:35:22 +0200

    etape 11

---
+++ test/services/bookServiceTest.js
@@ -0,0 +1,170 @@
+const { expect, sinon } = require('../testHelper')
+
+const bookService = require('../../lib/services/bookService')
+const bookRepository = require('../../lib/repositories/bookRepository')
+const Joi = require('@hapi/joi')
+const Book = require('../../lib/models').Book
+const Author = require('../../lib/models').Author
+
+describe('bookService', () => {
+
+  describe('create', () => {
+
+    let bookData
+    let bookCreationPromise
+
+    beforeEach(() => {
+      sinon.stub(bookRepository, 'create')
+    })
+
+    context('when the book data is valid', () => {
+
+      let book
+
+      beforeEach(() => {
+        // given
+        bookData = { title: 'les aventures de jjr', authorId: 1 }
+        book = new Book(bookData)
+        bookRepository.create.resolves(book)
+
+        // when
+        bookCreationPromise = bookService.create(bookData)
+      })
+
+      // then
+      it('should call the book Repository with the creation data', async () => {
+        // then
+        await bookCreationPromise.catch(() => {})
+        expect(bookRepository.create).to.have.been.calledWith(bookData)
+      })
+      it('should resolve with the created book from repository', () => {
+        // then
+        return expect(bookCreationPromise).to.eventually.equal(book)
+      })
+    })
+
+    context('when the book title is missing', () => {
+
+      beforeEach(() => {
+        // given
+        bookData = { title: undefined, authorId: 1 }
+
+        // when
+        bookCreationPromise = bookService.create(bookData)
+      })
+
+      it('should not call the book Repository', async () => {
+        // then
+        await bookCreationPromise.catch(() => {})
+        expect(bookRepository.create).to.not.have.been.called
+      })
+      it('should reject with a ValidationError error about missing title', () => {
+        // then
+        const expectedErrorDetails = [{
+          message: '"title" is required',
+          path: ['title'],
+          type: 'any.required',
+          context: { label: 'title', key: 'title' }
+        }]
+
+        return expect(bookCreationPromise)
+          .to.eventually.be.rejectedWith(Joi.ValidationError)
+          .with.deep.property('details', expectedErrorDetails)
+      })
+    })
+
+    context('when the book title is empty', () => {
+
+      beforeEach(() => {
+        // given
+        bookData = { title: '', authorId: 1 }
+
+        // when
+        bookCreationPromise = bookService.create(bookData)
+      })
+
+      it('should not call the book Repository', async () => {
+        // then
+        await bookCreationPromise.catch(() => {})
+        expect(bookRepository.create).to.not.have.been.called
+      })
+      it('should reject with a ValidationError error about empty title', () => {
+        // then
+        const expectedErrorDetails = [{
+          message: '"title" is not allowed to be empty',
+          path: ['title'],
+          type: 'string.empty',
+          context: { label: 'title', key: 'title', value: '' }
+        }]
+
+        return expect(bookCreationPromise)
+          .to.eventually.be.rejectedWith(Joi.ValidationError)
+          .with.deep.property('details', expectedErrorDetails)
+      })
+    })
+
+    context('when the author id is missing', () => {
+
+        beforeEach(() => {
+            // given
+            bookData = { title: 'La boucle', authorId: undefined }
+    
+            // when
+            bookCreationPromise = bookService.create(bookData)
+          })
+
+      it('should not call the book Repository', async () => {
+        // then
+        await bookCreationPromise.catch(() => {})
+        expect(bookRepository.create).to.not.have.been.called
+      })
+      it('should reject with a ValidationError error about missing book', () => {
+        // then
+        const expectedErrorDetails = [{
+          message: '"authorId" is required',
+          path: ['authorId'],
+          type: 'any.required',
+          context: { label: 'authorId', key: 'authorId' }
+        }]
+
+        return expect(bookCreationPromise)
+          .to.eventually.be.rejectedWith(Joi.ValidationError)
+          .with.deep.property('details', expectedErrorDetails)
+      })
+    })
+
+    context('when the author id is empty', () => {
+
+        beforeEach(() => {
+            // given
+            bookData = { title: 'Zinzin', authorId: '' }
+
+            // when
+            bookCreationPromise = bookService.create(bookData)
+        })
+
+      it('should not call the book Repository', async () => {
+        // then
+        await bookCreationPromise.catch(() => {})
+        expect(bookRepository.create).to.not.have.been.called
+      })
+      it('should reject with a ValidationError error about missing id', () => {
+          // then
+          const expectedErrorDetails = [{
+            message: '"authorId" must be a number',
+            path: ['authorId'],
+            type: 'number.base',
+            context: {
+              label: 'authorId', key: 'authorId', value: ''
+            },
+
+          }]
+
+          return expect(bookCreationPromise)
+            .to.eventually.be.rejectedWith(Joi.ValidationError)
+            .with.deep.property('details', expectedErrorDetails)
+        }
+      )
+    })
+  })
+})
