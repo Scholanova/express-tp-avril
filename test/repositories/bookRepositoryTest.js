81905833b2dc29d269374b108ef1b8df690eabc4
Pumma <paul.ppauwels@gmail.com>
2020-04-17 14:28:50 +0200

    Partie 3 - Etape 10

---
+++ test/repositories/bookRepositoryTest.js
@@ -0,0 +1,86 @@
+const { expect } = require('../testHelper')
+
+const bookRepository = require('../../lib/repositories/bookRepository')
+const authorRepository = require('../../lib/repositories/authorRepository')
+
+const models = require('../../lib/models')
+const { ResourceNotFoundError, SequelizeForeignKeyConstraintError } = require('../../lib/errors')
+const Book = models.Book
+const Author = models.Author
+
+
+describe('bookRepository', () => {
+
+  afterEach(async () => {
+    await Book.destroy({ where: {} })
+    await Author.destroy({ where: {} })
+  })
+
+  describe('Create book', () => {
+
+    let createdBook
+    let retrievedBook
+    let book
+
+    context('When create success, return a book', () => {
+
+        beforeEach(async () => {
+        // given
+        const jjr = { id: 1, name: 'Jean-Jacques Rousseau', pseudo: 'JJR', email: 'jj@rousseau.ch', language: 'french' }
+        authorRepository.create(jjr)
+        book = { title: 'Bankai', authorId: 1}
+
+        // when
+        createdBook = await bookRepository.create(book)
+        })
+
+        // then
+        it('Should return a book with the same title', async () => {
+        const createdBookValue = createdBook.get()
+
+        expect(createdBookValue.title).to.equal(book.title)
+        expect(createdBookValue.authorId).to.equal(book.authorId)
+
+        retrievedBook = await bookRepository.get(createdBook.id)
+        const retrievedBookValue = retrievedBook.get()
+
+        expect(createdBookValue).to.deep.equal(retrievedBookValue)
+        })
+    })
+
+    context('When create fail because no author associated to the book', () => {
+
+        beforeEach(async () => {
+        // given
+        bookData = { title: 'Zengetsu', authorId: 1}
+        })
+
+        // then
+        it('should return an SequelizeForeignKeyConstraintError:', async () => {
+            return expect(bookRepository.create(book)).to.eventually.be.rejectedWith(SequelizeForeignKeyConstraintError)
+        })
+    })
+  })
+
+  describe('Get book', () => {
+
+    let falsegId
+    let getBookPromise
+
+    context('Book does not exist', () => {
+      beforeEach(async () => {
+        // given
+        falsegId = 19
+
+        // when
+        getBookPromise = bookRepository.get(falsegId)
+      })
+
+      it('should throw a not found error', () => {
+        // then
+        return expect(getBookPromise).to.eventually.be.rejectedWith(ResourceNotFoundError)
+      })
+    })
+  })
+
+})
